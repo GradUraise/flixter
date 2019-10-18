55e1e1767ce9d6a854fd0a6c3001e2e33e9743ab
Melissa Roetker <mlroetker@yahoo.com>
2019-10-18 06:48:40 +0000

    User Enrolled or Not Enrolled message

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_authorized_for_current_lesson, only: [:show]
+
     def show
+        @lesson = current_lesson
     end
 
     private
+
+    def require_authorized_for_current_lesson
+        if !current_user.enrolled_in?(current_lesson.section.course)
+            redirect_to courses_path, alert: 'You are not enrolled in this course, yet'
+        end
+    end
 
     helper_method :current_lesson
     def current_lesson
