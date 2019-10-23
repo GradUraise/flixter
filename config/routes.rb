d5e16587597e91a8101260437217a3bc34d11da6
Melissa Roetker <mlroetker@yahoo.com>
2019-10-23 18:42:02 +0000

    updated routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,5 @@ Rails.application.routes.draw do
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
-    namespace :instructor do
-      resources :courses, only: [:new, :create, :show]
-    end
   end
 end
